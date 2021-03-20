2127894b264d224198b9e2b1913e5acf89a75adf
rickutino <rickutino@gmail.com>
2021-03-20 13:21:48 +0900

    add google styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --shape: #ffffff;
-    
+
     --red: #e52e4d;
     --blue: #5429cc;
     --blue-light: #6933ff;
@@ -31,6 +31,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Pippins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 600;
   }
 
   button {
