b07c4e5de3ca1783114522725b871dfdd3a1535f
rickutino <rickutino@gmail.com>
2021-03-21 01:31:15 +0900

    add style in NewTransactionModal

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,20 +1,50 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
-export const NewTransactionsModal: React.FC<NewTransactionModalProps> = ({ isOpen, onRequestClose }) => {
-  return (
-    <Container>
-        <Modal
-          isOpen={isOpen}
-          onRequestClose={onRequestClose}
+export const NewTransactionsModal: React.FC<NewTransactionModalProps> =
+  ({ isOpen, onRequestClose }) => {
+    return (
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+        <button
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
         >
-          <h2>Test</h2>
-        </Modal>
+          <img src={closeImg} alt="Close modal"/>
+        </button>
+        <Container>
+          <h2>Register Transaction</h2>
 
-    </Container>
-  )
+          <input type="text" placeholder="Title" />
+          <input type="number" placeholder="Value" />
+
+          <TransactionTypeContainer>
+            <button type="button">
+              <img src={incomeImg} alt="Income"/>
+              <span>Income</span>
+            </button>
+            <button type="button">
+              <img src={outcomeImg} alt="Outcome"/>
+              <span>Outcome</span>
+            </button>
+          </TransactionTypeContainer>
+
+          <input type="text" placeholder="Category" />
+
+          <button type="submit">Register</button>
+        </Container>
+      </Modal>
+    )
 }
