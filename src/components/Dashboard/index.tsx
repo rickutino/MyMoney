5a71f14527ab89eb6806559745a6fed8caad1e03
rickutino <rickutino@gmail.com>
2021-03-20 15:12:15 +0900

    add Summary Components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from '../Summary';
+import { Container } from './styles';
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}
