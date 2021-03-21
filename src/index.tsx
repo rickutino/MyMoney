0fef76cd05fe5626c9b90a3b349e7ac93ac48da8
rickutino <rickutino@gmail.com>
2021-03-21 19:31:05 +0900

    create seed MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,30 @@ createServer({
   models: {
     transaction: Model,
   },
-  
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "WEB制作案件",
+          type: 'deposit',
+          category: '開発',
+          amount: 120000,
+          createAt: new Date('2021-03-18 10:00:00'),
+        },
+        {
+          id: 2,
+          title: "マンションのローン",
+          type: 'withdraw',
+          category: 'ローン',
+          amount: 100000,
+          createAt: new Date('2021-04-05 11:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
