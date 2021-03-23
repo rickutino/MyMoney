22c9e89a310158b356fbb07a6f5ede1ce5d60035
rickutino <rickutino@gmail.com>
2021-03-23 22:03:07 +0900

    first release

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-  
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
